e9e3eb7a8a31b8e511003eb3c9b1e8d7b14460bf
DeShuai Ma <dma@redhat.com>
2016-04-05 17:25:33 +0800

    Update set_proxy.sh

--- set_proxy.sh
+++ set_proxy.sh
@@ -2,21 +2,27 @@
 # run this script in current shell example: 
 # "source set_proxy.sh example.proxy.com:3200" or ". set_proxy.sh example.proxy.com:3200"
 
+function set_proxy(){
+  typeset -u name
+  for name in http ftp https all
+  do
+    NAME=$name
+    if [ "$name" = "all" ]
+    then
+      export "$name"_proxy=socks://"$proxy_url"/
+      export "$NAME"_PROXY=socks://"$proxy_url"/
+    else
+      export "$name"_proxy=$name://"$proxy_url"/
+      export "$NAME"_PROXY=$name://"$proxy_url"/
+    fi
+  done
+  export no_proxy=localhost,127.0.0.0/8
+  export NO_PROXY=localhost,127.0.0.0/8
+}
+
 proxy_url=$1
-echo $proxy_url
-typeset -u NAME
-for name in http ftp https all
-do
-  NAME=$name
-  echo $NAME
-  if [ "$name" = "all" ]
-  then
-    export "$name"_proxy=socks://"$proxy_url"/
-    export "$NAME"_PROXY=socks://"$proxy_url"/
-  else
-    export "$name"_proxy=$name://"$proxy_url"/
-    export "$NAME"_PROXY=$name://"$proxy_url"/
-  fi
-done
-export no_proxy=localhost,127.0.0.0/8
-export NO_PROXY=localhost,127.0.0.0/8
+if [ ! "$proxy_url" ];then
+  echo "must set proxy"
+else
+  set_proxy
+fi
