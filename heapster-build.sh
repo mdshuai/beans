815a40057b636a643b95d8d6f9c9b28cf9fd11ea
DeShuai Ma <dma@redhat.com>
2016-08-01 14:34:51 +0800

    Update heapster build image

--- heapster-build.sh
+++ heapster-build.sh
@@ -5,3 +5,12 @@ go get github.com/tools/godep
 go get -d k8s.io/heapster
 cd $GOPATH/src/k8s.io/heapster
 make
+
+cd $GOPATH/src/k8s.io/heapster/ deploy/docker
+./build.sh
+
+cd $GOPATH/src/k8s.io/heapster/influxdb
+./build.sh
+
+cd $GOPATH/src/k8s.io/heapster/grafana
+./build.sh
