af9e9fa976567025c4e26268863ba5fe76990683
DeShuai Ma <dma@redhat.com>
2016-08-05 15:57:31 +0800

    Update heapster build image

--- heapster-build.sh
+++ heapster-build.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+#/bin/bash 
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:~/kubernetes/_output/bin/
 
+#deploy local k8s with kube-dns
+cd ~/
+git clone https://github.com/kubernetes/kubernetes.git
+cd kubernetes
+export KUBERNETES_PROVIDER=local
+export API_HOST=172.17.0.1
+export KUBE_ENABLE_CLUSTER_DNS=true
+hack/local-up-cluster.sh
+
+#build latest monitor binay and image
 export PATH=$PATH:$GOPATH/bin
 go get github.com/tools/godep
 go get -d k8s.io/heapster
@@ -14,3 +26,13 @@ cd $GOPATH/src/k8s.io/heapster/influxdb
 
 cd $GOPATH/src/k8s.io/heapster/grafana
 ./build.sh
+
+#deploy monitor
+cd $GOPATH/src/k8s.io/heapster
+sed -i -e 's/kubernetes\/heapster_influxdb:v0.5/kubernetes\/heapster_influxdb:canary/g' influxdb-grafana-controller.yaml
+sed -i -e 's/gcr.io/google_containers\/heapster_grafana:v2.6.0-2/gcr.io/google_containers\/heapster_grafana:canary/g' influxdb-grafana-controller.yaml
+kubectl create -f deploy/kube-config/influxdb/
+
+#check cluster-monitoring working
+sleep 10
+kubectl get pod --namespace=kube-system
