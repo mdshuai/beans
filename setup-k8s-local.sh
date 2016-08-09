4601024664a6b1977c7d4f3e5134745e4d540370
DeShuai Ma <dma@redhat.com>
2016-08-09 10:37:30 +0800

    Update setup-k8s-local.sh

--- setup-k8s-local.sh
+++ setup-k8s-local.sh
@@ -8,8 +8,8 @@ git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes
 export KUBERNETES_PROVIDER=local
 export KUBE_ENABLE_CLUSTER_DNS=true
-export KUBELET_HOST=172.18.12.44
-export API_HOST=172.17.0.1
-export HOSTNAME_OVERRIDE=172.18.12.44
+export KUBELET_HOST=172.18.12.44 #nodeip
+export API_HOST=172.17.0.1  #API_HOST=<ip of docker0>
+export HOSTNAME_OVERRIDE=172.18.12.44 #nodeip
 
 hack/local-up-cluster.sh
