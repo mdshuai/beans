77c083d62f52fc2cea905c535473bb73f43855fe
DeShuai Ma <dma@redhat.com>
2017-01-06 14:59:25 +0800

    Update heapster-deploy.sh

---
+++ heapster-deploy.sh
@@ -0,0 +1,34 @@
+#/bin/bash 
+
+#build latest monitor binay and image
+export PATH=$PATH:$GOPATH/bin
+go get github.com/tools/godep
+go get -d k8s.io/heapster
+cd $GOPATH/src/k8s.io/heapster
+make
+
+cd $GOPATH/src/k8s.io/heapster/deploy/docker
+./build.sh
+
+cd $GOPATH/src/k8s.io/heapster/influxdb
+./build.sh
+
+cd $GOPATH/src/k8s.io/heapster/grafana
+make
+
+#After finish build image, re-tag
+docker tag heapster:canary docker.io/ocpqe/heapster:canary
+docker tag heapster_influxdb:canary docker.io/ocpqe/heapster_influxdb:canary 
+docker tag gcr.io/google_containers/heapster_grafana:v3.1.1 docker.io/ocpqe/heapster_grafana:v3.1.1
+
+#deploy monitor
+cd $GOPATH/src/k8s.io/heapster/deploy/kube-config/influxdb/
+sed -i -e 's/kubernetes\/heapster:canary/docker.io\/ocpqe\/heapster:canary/g' heapster-deployment.yaml
+sed -i -e 's/kubernetes\/heapster_influxdb:v0.6/docker.io\/ocpqe\/heapster_influxdb:canary/g' influxdb-deployment.yaml
+sed -i -e 's/gcr.io\/google_containers\/heapster_grafana:v3.1.1/docker.io\/ocpqe\/heapster_grafana:v3.1.1/g' grafana-deployment.yaml
+kubectl create -f ./
+cd -
+
+#check cluster-monitoring working
+sleep 10
+kubectl get pod --namespace=kube-system
