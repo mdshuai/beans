1bf9757c800f53fe3ebb242f5bdb4d7def98d6b8
DeShuai Ma <dma@redhat.com>
2017-01-06 16:56:39 +0800

    Update setup-k8s-local.sh

--- setup-k8s-local.sh
+++ setup-k8s-local.sh
@@ -1,6 +1,6 @@
 #/bin/bash 
-export PATH=$PATH:/usr/local/go/bin
-export PATH=$PATH:~/kubernetes/_output/bin/
+#export PATH=$PATH:/usr/local/go/bin
+#export PATH=$PATH:~/kubernetes/_output/bin/
 
 #deploy local k8s with kube-dns
 cd ~/
@@ -9,7 +9,8 @@ cd kubernetes
 export KUBERNETES_PROVIDER=local
 export KUBE_ENABLE_CLUSTER_DNS=true
 export KUBELET_HOST=172.18.12.44 #nodeip
-export API_HOST=172.17.0.1  #API_HOST=<ip of docker0>
+#on need set API_HOST now.
+#export API_HOST=172.17.0.1  #API_HOST=<ip of docker0>
 export HOSTNAME_OVERRIDE=172.18.12.44 #nodeip
 
 hack/local-up-cluster.sh
