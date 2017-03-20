8d82bcb222fe7602e5ef66b85193112b8439f47c
DeShuai Ma <dma@redhat.com>
2017-03-20 18:09:37 +0800

    Update deploy-kube-up.sh

--- k8s/deploy-kube-up.sh
+++ k8s/deploy-kube-up.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+#Deploy latest k8s on gce by kube-up.sh
 #configure gcloud on host & test gcloud works well
+gcloud auth login
 gcloud config set project $project
-gcloud config set compute/zone us-central1-a
-gcloud compute instances create example-instance --machine-type n1-standard-1 --image debian-8
-gcloud compute --project $project ssh --zone "us-central1-a" "dma@example-instance"
-gcloud compute instances delete example-instance
+gcloud config set compute/zone us-central1-b
+#gcloud compute instances create example-instance --machine-type n1-standard-1 --image debian-8
+#gcloud compute --project $project ssh --zone "us-central1-a" "dma@example-instance"
+#gcloud compute instances delete example-instance
 
 #prepare the release
 git clone https://github.com/kubernetes/kubernetes.git
@@ -14,5 +15,9 @@ make quick-release
 
 #deploy k8s cluster on gce
 export KUBERNETES_PROVIDER=gce
+export INSTANCE_PREFIX=dma-
 export NUM_NODES=2
 ./cluster/kube-up.sh
+
+#When finish test please shutdown the env by
+./cluster/kube-down.sh
